e171e16903bfe9dd29f4014c73f094941ac70263
Doyun <materokatti@gmail.com>
2024-06-19 08:40:03 +0900

    feature : make ui of Game Screen

--- app/src/main/java/com/devstory/bullseye_game/GameScreen.kt
+++ app/src/main/java/com/devstory/bullseye_game/GameScreen.kt
@@ -1,11 +1,35 @@
 package com.devstory.bullseye_game
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.devstory.bullseye_game.ui.theme.BullseyeGameTheme
 
 @Composable
 fun GameScreen() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("PUT THE BULLSEYE AS YOU CAN TO")
+        Text(text = "89", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        Row {
+            Text(text = "1")
+            Slider(
+                value = 0.5f,
+                valueRange = 0.01f..1f,
+                onValueChange = {}
+            )
+            Text(text = "100")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "HIT ME")
+        }
+    }
 }
 
 @Preview(showBackground = true)
