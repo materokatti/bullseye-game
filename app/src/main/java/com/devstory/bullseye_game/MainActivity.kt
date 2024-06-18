1458095dca406c348fb7071b241f2d78b472fddd
Doyun <materokatti@gmail.com>
2024-06-18 21:17:00 +0900

    change Greeting message

--- app/src/main/java/com/devstory/bullseye_game/MainActivity.kt
+++ app/src/main/java/com/devstory/bullseye_game/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             BullseyeGameTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Doyun")
                 }
             }
         }
@@ -35,6 +35,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BullseyeGameTheme {
-        Greeting("Android")
+        Greeting("Doyun")
     }
 }
