0c5ef9a40c55bd2070ed6fe308b8c031c8deb749
Doyun <materokatti@gmail.com>
2024-07-12 10:16:19 +0900

    fix : modified the app to occupy the status bar area and navigation button area

--- app/src/main/java/com/devstory/bullseye_game/MainActivity.kt
+++ app/src/main/java/com/devstory/bullseye_game/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,6 +24,7 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             BullseyeGameTheme {
                 // A surface container using the 'background' color from the theme
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-    NavHost(navController, "game screen") {
+    NavHost(navController, "game screen", modifier = Modifier.padding(horizontal = 25.dp)) {
         composable("game screen") { GameScreen(
             onNavigationToAbout = {navController.navigate("about")}
         ) }
